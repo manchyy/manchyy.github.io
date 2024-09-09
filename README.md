ad23f44f1972d53ab75702360928ffb685c8466c
manchyy <nmantvis@gmail.com>
2024-09-09 16:38:39 +0100

    wipe

--- README.md
+++
@@ -1,4 +0,0 @@
-# manchyy.github.io
-First (proper) website of mine that I intend to use as a portfolio in the coming years.
-
-Preview: https://manchyy.github.io/
--- playSound.js
+++
@@ -1,9 +0,0 @@
-function playMusic()
-{
-    //var music = new Audio('manaBreak.mp3');
-    //music.play();
-
-    const origAudio = document.getElementById("manaBreak.mp3");
-    const newAudio = origAudio.cloneNode()
-    newAudio.play()
-}
